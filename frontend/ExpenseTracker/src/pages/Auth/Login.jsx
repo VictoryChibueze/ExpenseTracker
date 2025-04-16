3dccc866392181fd46c82e9c0abfe1bc2f9db048
Dr.Zeus⚡ <144681284+VictoryChibueze@users.noreply.github.com>
2025-04-16 10:43:07 +0100

    .

--- frontend/ExpenseTracker/src/pages/Auth/Login.jsx
+++ frontend/ExpenseTracker/src/pages/Auth/Login.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import AuthLayout from "../../components/Layout/AuthLayout";
 import { Link, useNavigate } from "react-router-dom";
 import Input from "../../components/input/input";
+import { validateEmail } from "../../utils/helper";
 
 const Login = () => {
   const [email, setEmail] = useState("");
