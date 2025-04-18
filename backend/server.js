25be9104ebfc250e222ba6d497aa47eba259670c
Dr.Zeus⚡ <144681284+VictoryChibueze@users.noreply.github.com>
2025-04-18 11:08:11 +0100

    .

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,19 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-
+const connectDB = require("./config/db");
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+app.use(express.json());
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
