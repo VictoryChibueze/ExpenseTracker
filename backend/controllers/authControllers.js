3903d4219d6f2c492f075721e4d02b428f4b0549
Dr.Zeus⚡ <144681284+VictoryChibueze@users.noreply.github.com>
2025-04-18 14:30:30 +0100

    .

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -8,7 +8,24 @@ const generateToken = (id) => {
 };
 
 //Register User
-exports.registerUser = async (req, res) => {};
+exports.registerUser = async (req, res) => {
+  const { fullName, email, password, profileImageUrl } = req.body;
+
+  //   Validation: Check for missing field
+
+  if (!fullName || !email || password) {
+    return res.status(400).json({ message: "All field are required" });
+  }
+
+  try{
+    //Check if email already exists
+
+    const existingUser = await User.findOne({email})
+    if(existingUser) {
+        return res.status(400).json({message:"Email already in use"})
+    }
+  }
+};
 
 //Login User
 exports.loginUser = async (req, res) => {};
