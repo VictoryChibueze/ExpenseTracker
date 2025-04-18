a8b745323d75dbd0179d951e8a3f08f9a65760ed
Dr.Zeus⚡ <144681284+VictoryChibueze@users.noreply.github.com>
2025-04-18 13:11:47 +0100

    .

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,1 +1,16 @@
-const jwt
+const jwt = require("jsonwebtoken");
+
+//GENERATE JWT Token
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+};
+
+//Register User
+exports.registerUser = async (req, res) => {};
+
+//Login User
+exports.loginUser = async (req, res) => {};
+
+//Get User Info
+exports.getUserInfo = async (req, res) => {};
