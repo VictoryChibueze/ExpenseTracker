db43e58d017497c819702936000ebbea00c44754
Dr.Zeus⚡ <144681284+VictoryChibueze@users.noreply.github.com>
2025-04-19 17:05:16 +0100

    .

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -13,37 +13,64 @@ exports.registerUser = async (req, res) => {
 
   //   Validation: Check for missing field
 
-  if (!fullName || !email || password) {
+  if (!fullName || !email || !password) {
     return res.status(400).json({ message: "All field are required" });
   }
 
-  try{
+  try {
     //Check if email already exists
 
-    const existingUser = await User.findOne({email})
-    if(existingUser) {
-        return res.status(400).json({message:"Email already in use"})
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already in use" });
     }
 
     // Create the user
 
     const user = await User.create({
-        fullName,
-        email,
-        password,
-        profileImageUrl
+      fullName,
+      email,
+      password,
+      profileImageUrl,
     });
 
-  res.status(201).json({
-    id:user._id,
-    user,
-    token:generateToken(user._id)
-  })
-
+    res.status(201).json({
+      id: user._id,
+      user,
+      token: generateToken(user._id),
+    });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ message: "Error registering user", error: err.message });
+  }
 };
 
 //Login User
-exports.loginUser = async (req, res) => {};
+exports.loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  if(!email || !password) {
+    return res.status(400).json({message:"All fields are required"})
+  }
+
+  try{
+    const user = await  User.findOne({email});
+    if(!user || !(await user.comparePassword(password))){
+      return res.status(400).json({message:"Invalid credentials"});
+    }
+    res.status(200).json({
+      id:user._id,
+      user,
+      token:generateToken(user._id)
+    })
+  }catch(err){
+    res
+      .status(500)
+      .json({ message: "Error registering user", error: err.message });
+
+  }
+};
 
 //Get User Info
 exports.getUserInfo = async (req, res) => {};
